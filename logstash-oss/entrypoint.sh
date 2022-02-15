ce269fb2ed760d2f5d40a9031b96df44295fcc41
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-02-15 18:01:45 +0100

    no message

--- logstash-oss/entrypoint.sh
+++ logstash-oss/entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 
+openssl req -x509 -batch -nodes -newkey rsa:4096 -keyout certs/tcp5050.pem.key -out certs/tcp5050.pem.crt -subj /CN=logstash-oss
+
+logstash
--- logstash/entrypoint.sh
+++ logstash/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-openssl req -x509 -batch -nodes -newkey rsa:4096 -keyout certs/tcp5050.key -out certs/tcp5050.cert -subj /CN=logstash
+openssl req -x509 -batch -nodes -newkey rsa:4096 -keyout certs/tcp5050.pem.key -out certs/tcp5050.pem.crt -subj /CN=logstash
+
+logstash

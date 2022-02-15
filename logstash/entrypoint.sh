2cc83d4ebe289547557c7470d499a13a7f4be67a
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2022-02-16 00:18:46 +0100

    no message

--- logstash/entrypoint.sh
+++ logstash/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cp -R /data/"${HOSTNAME}"/* certs
+while true ; do
+  [ -f "/data/${HOSTNAME}.crt.pem" ] && [ -f "/data/${HOSTNAME}.key.pem" ] && break
+  sleep 1
+done
+
+cp /data/ca.crt.pem ./certs/
+cp /data/"${HOSTNAME}".crt.pem ./certs/
+cp /data/"${HOSTNAME}".key.pem ./certs/
 
 logstash
